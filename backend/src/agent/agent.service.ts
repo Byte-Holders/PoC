187f0d0d0451623daf5fe4c922672ed93a0e8c76
Okonigiri <207894241+Okonigiri@users.noreply.github.com>
2026-02-04 19:05:12 +0100

    Chiamata a ChatBedrockConverse

--- backend/src/agent/agent.service.ts
+++ backend/src/agent/agent.service.ts
@@ -2,19 +2,28 @@ import { Injectable } from '@nestjs/common';
 import path from 'path';
 import fs from 'fs';
 import { execSync } from 'child_process';
-import { StateGraph, START, END, Annotation } from '@langchain/langgraph';
+import { StateGraph, START, END, Annotation, GraphNode } from '@langchain/langgraph';
 import { SystemMessage, HumanMessage } from '@langchain/core/messages';
 import { ChatBedrockConverse } from '@langchain/aws';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const AgentState = Annotation.Root({
-  reportPath: Annotation<string>(),
+  reportPath: Annotation<string | unknown>(),
   analysis: Annotation<string>(),
 });
 
+export type ModelCreateInfo = {
+  name: string,
+  region?: string,
+  temperature?: number,
+  maxTokens?: number,
+}
+
 @Injectable()
 export class AgentService {
-
-  async runSemgrepScan() {
+  async runSemgrepScan(): Promise<string | unknown> {
     const dateStr = new Date().toISOString().replace(/[:.]/g, '-');
     const reportPath = path.resolve(`./reports/test_scan_${dateStr}.json`);
 
@@ -49,14 +58,19 @@ export class AgentService {
         },
       );
       console.log(`Scansione completata. File salvato in: ${reportPath}`);
-    } catch (error) {
+      return reportPath;
+    } catch (error: unknown) {
       console.error("Errore durante l'esecuzione di Semgrep:", error);
+      return error;
     }
   }
 
   // workflow
 
   async execute() {
+    const model = this.createModel({
+      name: "qwen.qwen3-coder-30b-a3b-v1:0"
+    });
     const workflow = new StateGraph(AgentState)
 
       // Nodo 1: Esegue la tua scansione
@@ -66,9 +80,11 @@ export class AgentService {
       })
 
       .addNode('ai_analysis', async (state) => {
-        const fullJsonRaw = fs.readFileSync(state.reportPath, 'utf-8');
+        // non gestisco errori
+        const fullJsonRaw = fs.readFileSync(state.reportPath as string, 'utf-8');
 
-        const response = await this.model.invoke([
+        console.log('Chiamata al modello');
+        const response = await model.invoke([
           new SystemMessage(
             `Crea un report dettagliato e discorsivo(non elenco puntato) del file report restituito da Semgrep, facendo notare le vulnerabilita' piu critiche`,
           ),
@@ -76,6 +92,8 @@ export class AgentService {
             `Ecco il file JSON integrale della scansione: \n\n ${fullJsonRaw}`,
           ),
         ]);
+
+        console.log(`Riassunto generato: ${response.content as string}`);
 
         return { analysis: response.content as string };
       })
@@ -86,6 +104,16 @@ export class AgentService {
 
     const app = workflow.compile();
 
-    return await app.invoke({});
+    return (await app.invoke({})).analysis;
+  }
+
+  private createModel(modelCI: ModelCreateInfo) {
+    console.log(`Creazione llm: ${modelCI.name}`);
+    return new ChatBedrockConverse({
+      model: modelCI.name,
+      region: process.env.BEDROCK_AWS_REGION || modelCI.region || 'eu-north-1',
+      temperature: modelCI.temperature || 0,
+      maxTokens: modelCI.maxTokens || 1000,
+    });
   }
 }
