0fa639f8388f4c1d5ebfa7fc4755c32154da9d08
NicoloLattanzio <nicololattanzio@gmail.com>
2026-02-04 12:24:57 +0100

    Commit aggiornati i get nel controller e modificato dockerfile per dipendenze semgrep

--- backend/src/agent/agent.controller.ts
+++ backend/src/agent/agent.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from "@nestjs/common";
-// import { AgentService } from "./agent.service";
+import { Controller, Get, Redirect } from "@nestjs/common";
+import { AgentService } from "./agent.service";
 import * as mongoose from "mongoose";
+import * as path from 'path';
+import * as fs from 'fs';
 
 class ReportDto {
   name: string;
@@ -16,36 +18,53 @@ const reportDtoSchema = new mongoose.Schema<ReportDto>({
 
 const reportModel = mongoose.model('report', reportDtoSchema);
 
-@Controller("agent")
+@Controller('agent')
 export class AgentController {
-  constructor(/* private readonly agentService: AgentService */) { }
-
-  @Get()
-  async testConnection() {
-    const newReport = new reportModel({
-      name: "Nome report",
-      description: "Descrizione report",
-      date: new Date(),
-    });
-
-    const saveResult = await (async () => {
-      try {
-        await newReport.save();
-        return "Salvato il report";
-      }
-      catch (e: unknown) {
-        if (e instanceof Error)
-          return "Errore salvataggio report (e: Error)";
-
-        return "Errore salvataggio report (e: not Error)";
-      }
-    })();
-
-    return saveResult;
-  }
+  constructor(private readonly AgentService: AgentService) {}
 
   @Get('results')
   async findReports() {
-    return await reportModel.find();
+    return reportModel.find();
+  }
+
+  @Get('scan')
+  @Redirect()
+  async getScan() {
+    await this.AgentService.runSemgrepScan();
+    const reportsDir = path.resolve('./reports');
+
+    try {
+      if (!fs.existsSync(reportsDir)) return "Cartella reports non trovata";
+      const files = fs.readdirSync(reportsDir)
+
+      if (files.length === 0) return "Nessun file di report trovato";
+
+      const latestFile = path.join(reportsDir, files[0]);
+
+      // 2. Leggi e Parsea il JSON di Semgrep
+      const rawData = fs.readFileSync(latestFile, 'utf-8');
+      const semgrepData = JSON.parse(rawData);
+
+      const results = semgrepData.results;
+
+      if (!results || results.length === 0) {
+        return "Il report è valido ma non contiene vulnerabilità (results vuoto).";
+      }
+      //Prendiamo la prima vulnerabilità trovata
+      const firstIssue = results[0];
+
+      // Mappo i campi dati del json
+      const newReport = new reportModel({
+        name: firstIssue.check_id,
+        description: firstIssue.extra?.message || "Nessuna descrizione",
+        date: new Date(),
+      });
+
+      await newReport.save();
+
+    } catch (e) {
+      return e instanceof Error ? e.message : "Errore";
+    }
+     return { url: `../agent/results` };
   }
 }
--- backend/src/agent/agent.service.ts
+++ backend/src/agent/agent.service.ts
@@ -1,7 +1,51 @@
 import { Injectable } from "@nestjs/common";
-import { HttpService } from "@nestjs/axios";
+//import { HttpService } from "@nestjs/axios";
+import path from 'path';
+import fs from 'fs';
+import { execSync } from 'child_process';
 
 @Injectable()
 export class AgentService {
-  constructor(/* private readonly httpService: HttpService */) { }
+  constructor(/* private readonly httpService: HttpService */) {}
+
+  dateStr = new Date().toISOString().replace(/[:.]/g, '-');
+  reportPath = path.resolve(`./reports/test_scan_${this.dateStr}.json`);
+
+  async runSemgrepScan() {
+    console.log('Avvio test Semgrep...');
+
+    //Se non esiste la cartella report la crea
+    const outputDir = path.dirname(this.reportPath);
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+      console.log(`Creo la cartella: ${outputDir}`);
+    }
+
+    try {
+      // Se non trova semgrep non avvia il comando
+      try {
+        const version = execSync('semgrep --version').toString().trim();
+        console.log(`Semgrep trovato: versione ${version}`);
+      } catch (err) {
+        console.error('Semgrep non sembra essere installato o nel PATH.');
+        throw new Error('Semgrep non e stato trovato');
+      }
+
+      //Avvia la scansione su projectRoot che pero si cambia easy se serve fare la scansione su qualcosa di diverso.
+      const projectRoot = process.cwd();
+      console.log(`Scansione in corso su: ${projectRoot}`);
+
+      execSync(
+        `semgrep scan ${projectRoot} --config auto --json --output ${this.reportPath} --exclude=node_modules --exclude=reports --exclude=dist --quiet`,
+        {
+          stdio: 'inherit',
+          encoding: 'utf-8',
+        },
+      );
+      console.log(`Scansione completata. File salvato in: ${this.reportPath}`);
+
+    } catch (error) {
+      console.error("Errore durante l'esecuzione di Semgrep:", error);
+    }
+  }
 }
